d662be117fa06ffaa970e34780e042cdbed4c3ad
Marco <2om.marco.quintella@gmail.com>
2019-04-20 23:52:58 -0300

    commit

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -23,18 +23,12 @@ export default {
     }
   },
   async created () {
-    for (let i = 1; i <= 30; i++) {
+    for (let i = 1; i <= 1; i++) {
       let today = ''
       i <= 9 ? (today = '0' + i) : (today = i)
-      this.data = {
-        startDate: '04/' + today + '/2019',
-        endDate: '04/30/2019',
-        tipoEvento: 'Todos'
-      }
-      await Axios.post(
-        'https://www.ccee.org.br/portal/faces/oracle/webcenter/portalapp/pages/publico/noticiaopiniao/eventos_calendario.jspx',
-        {
-          data: this.data }
+      today = '04/' + today + '/2019'
+      await Axios.get(
+        'https://www.ccee.org.br/portal/faces/oracle/webcenter/portalapp/pages/publico/noticiaopiniao/eventos_calendario.jspx?startDate=' + today
       )
         .then(response => {
           this.calendario = this.calendario + '<br><br>' + response.data
